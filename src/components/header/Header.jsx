73f3bb771d2e97dbf52ebba1cf718adeaa4465fc
Jonathan Puglla <jonathan.puglla@outlook.com>
2023-04-17 10:55:29 -0500

    Feat: Home Component
    Home Component is done, added the Home.jsx, ScrollDown.jsx, Social.jsx files for the component and their stylesheets: home.css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
     <header className="header">
         <nav className="nav container">
             <a href="index.html" className="nav__logo">
-                Jonathan
+                Puglla
             </a>
 
             <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
