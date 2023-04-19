e326a7b7896c5f2954903d8c98f4ba18341d5a26
Jonathan Puglla <jonathan.puglla@outlook.com>
2023-04-19 13:25:53 -0500

    Feat: Footer Component
    Footer component is done. Inside the footer folder the stylesheet and the Footer.jsx files were added.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,28 +1,54 @@
 import React from 'react';
-
+import './footer.css';
 
 const Footer = () => {
-  return (
-    <div className="footer">
-        <div className="footer__container container">
-            <h1 className="footer__title">Puglla</h1>
+    return (
+        <footer className="footer">
+            <div className="footer__container container">
+                <h1 className="footer__title">Puglla</h1>
 
-            <ul className="footer__list">
-                <li>
-                    <a href="#about" className="footer__link">About</a>
-                </li>
-                <li>
-                    <a href="#portfolio" className="footer__link">Projects</a>
-                </li>
-                <li>
-                    <a href="#contact" className="footer__link">Contacts</a>
-                </li>
-            </ul>
+                <ul className="footer__list">
+                    <li>
+                        <a href="#about" className="footer__link">About</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio" className="footer__link">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#contact" className="footer__link">Contacts</a>
+                    </li>
+                </ul>
 
-            <div className="footer__social"></div>
-        </div>
-    </div>
-  )
+                <div className="footer__social">
+                    <a
+                        href="https://twitter.com/Jonathan2525252"
+                        className="footer__social-link"
+                        target='_blank'
+                    >
+                        <i class="bx bxl-twitter"></i>
+                    </a>
+
+                    <a
+                        href="https://linkedin.com/in/jonapuglla2000"
+                        className="footer__social-link"
+                        target='_blank'
+                    >
+                        <i class="bx bxl-linkedin"></i>
+                    </a>
+
+                    <a
+                        href="https://github.com/jonap22"
+                        className="footer__social-link"
+                        target='_blank'
+                    >
+                        <i class="bx bxl-github"></i>
+                    </a>
+                </div>
+
+                <span className="footer__copy">&#169; 2023. Made with love.</span>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
