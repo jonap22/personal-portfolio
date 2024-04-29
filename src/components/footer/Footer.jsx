8a84ccaaa021c93abb2a9efb94c50f85b594e062
Jonathan Puglla <76792472+jonap22@users.noreply.github.com>
2024-04-29 17:03:36 -0500

    Jp/development (#3)
    * chore: Update CV to 16 Dec 2023
    
    * chore: Update qualification status.
    
    * chore: Update copyright status.
    
    * feat: Update to "Engineer" on every component.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,57 +1,63 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="footer__container container">
-                <h1 className="footer__title">Puglla</h1>
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title">Puglla</h1>
 
-                <ul className="footer__list">
-                    <li>
-                        <a href="#about" className="footer__link">About</a>
-                    </li>
-                    <li>
-                        <a href="#portfolio" className="footer__link">Projects</a>
-                    </li>
-                    <li>
-                        <a href="#contact" className="footer__link">Contacts</a>
-                    </li>
-                </ul>
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio" className="footer__link">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="footer__link">
+              Contacts
+            </a>
+          </li>
+        </ul>
 
-                <div className="footer__social">
-                    <a
-                        href="https://twitter.com/Jonathan2525252"
-                        className="footer__social-link"
-                        rel="noreferrer"
-                        target='_blank'
-                    >
-                        <i className="fa-brands fa-x-twitter"></i>
-                    </a>
+        <div className="footer__social">
+          <a
+            href="https://twitter.com/Jonathan2525252"
+            className="footer__social-link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="fa-brands fa-x-twitter"></i>
+          </a>
 
-                    <a
-                        href="https://linkedin.com/in/jonapuglla2000"
-                        className="footer__social-link"
-                        rel="noreferrer"
-                        target='_blank'
-                    >
-                        <i className="bx bxl-linkedin"></i>
-                    </a>
+          <a
+            href="https://linkedin.com/in/jonapuglla2000"
+            className="footer__social-link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="bx bxl-linkedin"></i>
+          </a>
 
-                    <a
-                        href="https://github.com/jonap22"
-                        className="footer__social-link"
-                        rel="noreferrer"
-                        target='_blank'
-                    >
-                        <i className="bx bxl-github"></i>
-                    </a>
-                </div>
+          <a
+            href="https://github.com/jonap22"
+            className="footer__social-link"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <i className="bx bxl-github"></i>
+          </a>
+        </div>
 
-                <span className="footer__copy">&#169; 2023. Made with love.</span>
-            </div>
-        </footer>
-    )
-}
+        <span className="footer__copy">&#169; 2024. Made with love.</span>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,187 +1,166 @@
-import React, { useState } from 'react'
-import "./qualification.css"
+import React, { useState } from "react";
+import "./qualification.css";
 
 const Qualification = () => {
-    const [toggleState, setToggleState] = useState(1);
+  const [toggleState, setToggleState] = useState(1);
 
-    const toggleTab = (index) => {
-        setToggleState(index);
-    };
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
 
-    return (
-        <section className="qualification section" id="qualification">
-            <h2 className="section__title">Qualification</h2>
-            <span className="section__subtitle">My personal journey</span>
+  return (
+    <section className="qualification section" id="qualification">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My personal journey</span>
 
-            <div className="qualification__container container">
-                <div className="qualification__tabs">
-                    <div
-                        className={
-                            toggleState === 1
-                                ? "qualification__button qualification__active button--flex"
-                                : "qualification__button button--flex"
-                        }
-                        onClick={() => toggleTab(1)}
-                    >
-                        <i className="uil uil-graduation-cap qualification__icon"></i>
-                        Education
-                    </div>
+      <div className="qualification__container container">
+        <div className="qualification__tabs">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
+            <i className="uil uil-graduation-cap qualification__icon"></i>
+            Education
+          </div>
 
-                    <div
-                        className={
-                            toggleState === 2
-                                ? "qualification__button qualification__active button--flex"
-                                : "qualification__button button--flex"
-                        }
-                        onClick={() => toggleTab(2)}
-                    >
-                        <i className="uil uil-briefcase-alt qualification__icon"></i>
-                        Experience
-                    </div>
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
+            <i className="uil uil-briefcase-alt qualification__icon"></i>
+            Experience
+          </div>
+        </div>
+        <div className="qualification__sections">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Software Engineering</h3>
+                <span className="qualification__subtitle">EPN - Ecuador</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> 2019 - 2024
                 </div>
-                <div className="qualification__sections">
-                    <div
-                        className={
-                            toggleState === 1
-                                ? "qualification__content qualification__content-active"
-                                : "qualification__content"
-                        }
-                    >
-                        <div className="qualification__data">
-                            <div>
-                                <h3 className="qualification__title">
-                                    Software Engineering
-                                </h3>
-                                <span className="qualification__subtitle">
-                                    EPN - Ecuador
-                                </span>
-                                <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>  2019 - Present
-                                </div>
-                            </div>
+              </div>
 
-                            <div>
-                                <span className="qualification__rounder">
-
-                                </span>
-                                <span className="qualification__line">
-
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div
-                        className={
-                            toggleState === 2
-                                ? "qualification__content qualification__content-active"
-                                : "qualification__content"
-                        }
-                    >
-                        <div className="qualification__data">
-                            <div>
-                                <h3 className="qualification__title">
-                                    Teaching Assistant
-                                </h3>
-                                <span className="qualification__subtitle">
-                                    EPN - Ecuador
-                                </span>
-                                <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>  Jun 2022 - Sep 2022
-                                </div>
-                            </div>
-
-                            <div>
-                                <span className="qualification__rounder">
-
-                                </span>
-                                <span className="qualification__line">
-
-                                </span>
-                            </div>
-                        </div>
-
-                        <div className="qualification__data">
-                            <div>
-
-                            </div>
-
-                            <div>
-                                <span className="qualification__rounder">
-
-                                </span>
-                                <span className="qualification__line">
-
-                                </span>
-                            </div>
-
-                            <div>
-                                <h3 className="qualification__title">
-                                    Computer Laboratory Assistant
-                                </h3>
-                                <span className="qualification__subtitle">
-                                    EPN - Ecuador
-                                </span>
-                                <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>  Apr 2022 - Apr 2023
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="qualification__data">
-                            <div>
-                                <h3 className="qualification__title">
-                                    Cybersecurity Intern
-                                </h3>
-                                <span className="qualification__subtitle">
-                                    Fextor - Ecuador
-                                </span>
-                                <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>  Dec 2022 - Present
-                                </div>
-                            </div>
-
-                            <div>
-                                <span className="qualification__rounder">
-
-                                </span>
-                                <span className="qualification__line">
-
-                                </span>
-                            </div>
-                        </div>
-
-                                                <div className="qualification__data">
-                            <div>
-
-                            </div>
-
-                            <div>
-                                <span className="qualification__rounder">
-
-                                </span>
-                                <span className="qualification__line">
-
-                                </span>
-                            </div>
-
-                            <div>
-                                <h3 className="qualification__title">
-                                    Software Developer Intern
-                                </h3>
-                                <span className="qualification__subtitle">
-                                    Stack Builders
-                                </span>
-                                <div className="qualification__calendar">
-                                    <i className="uil uil-calendar-alt"></i>  Jul 2023 - Present
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
             </div>
-        </section>
-    )
-}
+          </div>
 
-export default Qualification
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Teaching Assistant</h3>
+                <span className="qualification__subtitle">EPN - Ecuador</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> Jun 2022 - Sep 2022
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">
+                  Computer Laboratory Assistant
+                </h3>
+                <span className="qualification__subtitle">EPN - Ecuador</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> Apr 2022 - Sep 2023
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Cybersecurity Intern</h3>
+                <span className="qualification__subtitle">
+                  Fextor - Ecuador
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> Dec 2022 - Apr 2024
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">
+                  Software Developer Intern
+                </h3>
+                <span className="qualification__subtitle">Stack Builders</span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> Jul 2023 - Dec 2023
+                </div>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Software Analyst II</h3>
+                <span className="qualification__subtitle">
+                  Ecuadorian Army - Ecuador
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calendar-alt"></i> Apr 2024 - Present
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Qualification;
